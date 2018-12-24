e12c3d17df4bd21996223193d1d55d89f5ebddd8
revote <revote@netcourrier.com>
2018-12-24 17:49:10 +0100

    A bit code

--- src/app.js
+++ src/app.js
@@ -2,8 +2,21 @@ const {Button, TextView, ui} = require('tabris');
 
 let button = new Button({
   centerX: 0, top: 100,
-  text: 'Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message Hello message '
+  text: 'Hello Revote '
 }).appendTo(ui.contentView);
+
+
+let buttonCheckPermission = new Button({
+  centerX: 0, top: 200,
+  text: 'Check Permission '
+}).appendTo(ui.contentView);
+
+
+let buttonRequestPermission = new Button({
+  centerX: 0, top: 300,
+  text: 'Ask for Permission '
+}).appendTo(ui.contentView);
+
 
 let textView = new TextView({
   centerX: 0, top: [button, 50],
@@ -12,4 +25,35 @@ let textView = new TextView({
 
 button.on('select', () => {
   textView.text = 'Tabris.js rocks!';
+   window.plugins.sim.getSimInfo(successCallback, errorCallback);
 });
+
+buttonCheckPermission.on('select', () => {
+  textView.text = 'Checkingd perm!';
+   hasReadPermission();
+});
+
+buttonRequestPermission.on('select', () => {
+  textView.text = 'Checkingd perm!';
+   requestReadPermission();
+});
+
+
+function successCallback(result) {
+  console.log(result);
+}
+
+function errorCallback(error) {
+  console.log(error);
+}
+
+
+// Android only: check permission
+function hasReadPermission() {
+  window.plugins.sim.hasReadPermission(successCallback, errorCallback);
+}
+
+// Android only: request permission
+function requestReadPermission() {
+  window.plugins.sim.requestReadPermission(successCallback, errorCallback);
+}
